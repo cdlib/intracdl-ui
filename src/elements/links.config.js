f8c4220dadf20d7ab91ddada8e1d7b4e39486730
JoelCDL <joel.hagedorn@ucop.edu>
2023-03-01 14:38:55 -0800

    Create link element styling

---
+++ src/elements/links.config.js
@@ -0,0 +1,14 @@
+const { faker } = require('@faker-js/faker/locale/en');
+
+module.exports = {
+  label: 'Links',
+  collated: true,
+  context: {
+    link: {
+      text: faker.lorem.words(5)
+    },
+    link1: {
+      text: faker.lorem.words(5)
+    }
+  }
+}
