b5361f98f8a6bcd09638a1e317c31b1dc95bb0b9
JoelCDL <joel.hagedorn@ucop.edu>
2023-03-07 16:20:37 -0800

    Create post date component

---
+++ src/components/postdate.config.js
@@ -0,0 +1,15 @@
+const { faker } = require('@faker-js/faker/locale/en')
+const fakerDate = faker.date.past()
+const dateUnformatted = new Date(fakerDate)
+const dateFormat = {year: 'numeric', month: 'long', day: 'numeric'} // example: April 12, 2022
+const postDate = dateUnformatted.toLocaleDateString('en-US', dateFormat)
+
+module.exports = {
+  label: 'Post Date',
+  context: {
+    postdate: {
+      name: faker.name.fullName(),
+      date: postDate
+    }
+  }
+}
