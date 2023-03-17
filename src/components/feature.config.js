6038e028ea8ad5701d7af5e0aba10e3573311b48
JoelCDL <joel.hagedorn@ucop.edu>
2023-03-17 16:44:38 -0700

    Recreate feature component

--- src/components/feature.config.js
+++ src/components/feature.config.js
@@ -1,3 +1,11 @@
+const { faker } = require('@faker-js/faker/locale/en')
+
 module.exports = {
-  label: 'Media Feature'
+  label: 'Feature',
+  context: {
+    feature: {
+      caption: faker.lorem.sentence(8),
+      image: faker.image.nature(350, 200)
+    }
+  }
 }
