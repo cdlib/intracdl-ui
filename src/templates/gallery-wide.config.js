68bd231de6afdd18250c3ffe9daf0640fd9138ea
JoelCDL <joel.hagedorn@ucop.edu>
2023-05-24 16:00:22 -0700

    Create gallery templates

---
+++ src/templates/gallery-wide.config.js
@@ -0,0 +1,20 @@
+const { faker } = require('@faker-js/faker/locale/en')
+const cardData = []
+
+for (var i = 0; i < 15; i++) {
+  cardData.push({
+    card: {
+      title: faker.commerce.productName(),
+      image: faker.image.nature(225, 125, true),
+      caption: faker.lorem.sentence(),
+    }
+  })
+}
+
+module.exports = {
+  label: 'Gallery Wide',
+  collated: true,
+  context: {
+    card: cardData
+  }
+}
