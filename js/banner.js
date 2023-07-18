93c3d19661f2f5cd27d052aaa665e1653798a3b8
JoelCDL <joel.hagedorn@ucop.edu>
2023-07-17 17:39:05 -0700

    Add cross fade transition to banner component when hidden

--- js/banner.js
+++ js/banner.js
@@ -1,10 +1,15 @@
 // Banner Component //
 
+import { crossFadeTransition } from './transitions.js'
 const banner = document.querySelector('.js-banner')
 const bannerButton = document.querySelector('.js-banner button')
 
+const bannerHide = () => {
+  banner.hidden = true
+}
+
 if (banner) {
   bannerButton.addEventListener('click', () => {
-    banner.hidden = true
+    crossFadeTransition(bannerHide)
   })
 }
