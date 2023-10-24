24e2644bbbb57e955640574559deb6d6b619a16c
JoelCDL <joel.hagedorn@ucop.edu>
2023-10-24 16:50:11 -0700

    Replace interior nav js with imported nav toggle module

--- js/interiornav.js
+++ js/interiornav.js
@@ -1,41 +1,11 @@
-// Interior Nav Component Toggle //
+// Interior Navigation Component //
 
+import { navToggle as interiorNavToggle } from './navtoggle.js'
 const interiorNav = document.querySelector('.js-interiornav')
-const navButton = document.querySelector('.js-interiornav button')
-const navList = document.querySelector('.js-interiornav__list')
-const navMediaQuery = window.matchMedia('(width > 50rem)')
 
 if (interiorNav) {
-  const navOpen = () => {
-    navButton.setAttribute('aria-expanded', true)
-    navButton.setAttribute('aria-label', 'close')
-    navList.hidden = false
-  }
+  const navButton = document.querySelector('.js-interiornav button')
+  const navList = document.querySelector('.js-interiornav__list')
 
-  const navClose = () => {
-    navButton.setAttribute('aria-expanded', false)
-    navButton.setAttribute('aria-label', 'open')
-    navList.hidden = true
-  }
-
-  navButton.addEventListener('click', () => {
-    if (navButton.getAttribute('aria-expanded') === 'false') {
-      navOpen()
-    } else {
-      navClose()
-    }
-  })
-
-  const navMediaQueryToggle = e => {
-    if (e.matches) {
-      navButton.hidden = true
-      navOpen()
-    } else {
-      navButton.hidden = false
-      navClose()
-    }
-  }
-
-  navMediaQuery.addEventListener('change', navMediaQueryToggle)
-  navMediaQueryToggle(navMediaQuery)
+  interiorNavToggle(navButton, navList)
 }
