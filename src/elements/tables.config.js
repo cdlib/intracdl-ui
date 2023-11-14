c39731e8d97c231903b31963541301dd2fb42242
JoelCDL <joel.hagedorn@ucop.edu>
2023-11-14 15:50:21 -0800

    Create table element styles

---
+++ src/elements/tables.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  label: 'Tables',
+  notes: 'This table simulates tables created in the WordPress table block editor, which are contained in a figure element and may have a figcaption for the table caption.'
+}
