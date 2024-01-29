e43194e3c8c95360021361469a063c0348c7471f
JoelCDL <joel.hagedorn@ucop.edu>
2024-01-29 12:11:56 -0800

    Create vimeo full width component

---
+++ src/components/vimeo-full-width.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  label: 'Vimeo Full Width',
+  context: {
+    vimeo: {
+      id: '854502345',
+      caption: 'WEST Deselection Resources Workshop',
+      link: {
+        text: 'Meeting Agenda',
+        url: 'https://cdlib.org'
+      }
+    }
+  }
+}
