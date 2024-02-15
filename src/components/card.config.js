61eb11e03e41b41c9ecf76b622bc141cc251aba9
JoelCDL <joel.hagedorn@ucop.edu>
2024-02-15 14:30:40 -0800

    Create second card variant with no caption and added link text

--- src/components/card.config.js
+++ src/components/card.config.js
@@ -2,6 +2,7 @@ const { faker } = require('@faker-js/faker/locale/en')
 
 module.exports = {
   label: 'Card',
+  collated: true,
   context: {
     card: {
       title: faker.commerce.productName(),
@@ -10,5 +11,19 @@ module.exports = {
       caption: faker.lorem.paragraph(),
       link2: faker.commerce.productName()
     }
-  }
+  },
+  variants: [
+    {
+      name: '2', // virtually creates card--2.hbs
+      context: {
+        card: {
+          title: faker.commerce.productName(),
+          image: faker.image.nature(225, 125),
+          link1: 'Logo Guidelines',
+          caption: '',
+          link2: 'Download ZIP (EPS, PNG, SVG)'
+        }
+      }
+    }
+  ]
 }
--- src/templates/gallery-narrow-logos.config.js
+++ src/templates/gallery-narrow-logos.config.js
@@ -4,11 +4,12 @@ const cardData = []
 
 for (const logo of logos) {
   cardData.push({
-    logo: {
-      institution: faker.commerce.productName(),
+    card: {
+      title: faker.commerce.productName(),
       image: 'https://files.axshare.com/gsc/MWNJ3F/51/04/b5/5104b527ebd5496eb53f6a198127037a/images/interior-page_logo-index_campuses/p' + logo + '.svg?pageId=b295d073-b030-4e99-b185-d8d2b84fc8fb',
-      guidelines: 'Logo Guidelines',
-      download: 'Download ZIP (EPS, PNG, SVG)'
+      link1: 'Logo Guidelines',
+      caption: '',
+      link2: 'Download ZIP (EPS, PNG, SVG)'
     }
   })
 }
